c899972bfd19881a05120ce4314e2b9da8b351e0
Mariam Hasanat <hasanattmariam@gmail.com>
2024-12-30 21:24:25 +0200

    run the server without errors

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded());
 app.use(express.json());
 
 // Cors for cross origin allowance
 app.use(cors())
 
 // Initialize the main project folder
-app.use(express.static('website'));
+// app.use(express.static('website'));
 
 
 // Setup Server
 
 const port = 8080;
+// const url = 'http://localhost:8080';
 
-const server = app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-    
+
 });
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello from Mariam Server baad ma telet rohaa")
+})
+
+app.get('/getData', (req, res) => {
+    // console.log(projectData);
+
+    try {
+        // const comingData = JSON.parse(req.body);
+        // projectData = {...projectData, comingData}
+        // console.log(projectData);
+        // const data = JSON.stringify(projectData);
+        console.log("Hello from server");
+
+        res.status(200).send(projectData)
+    } catch (error) {
+        console.error('error happened in get response', error)
+    }
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,24 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '5b5c9875d7e328484f0b89d44e752635&units=imperial';
+const url = 'http://localhost:8080';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const retrieveData = async (url) => {
+    const commingData = await fetch(url+'/getData', {
+        method: 'GET'
+    });
+    try {
+        // const response  = await commingData.json();
+        console("retrieved data: ", commingData)
+        return commingData
+    } catch (error) {
+        console.log("error in fetch the data in retrieve data function ", error)
+    }
+}
+
+console.log(retrieveData(url));
+ 
