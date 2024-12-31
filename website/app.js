9c359fcaa8cfef89a01c3d734c4efc6d510e520e
Mariam Hasanat <hasanattmariam@gmail.com>
2024-12-31 21:46:01 +0200

    remove unnecessary console.log add catch for each .then check if the user didn't enter the zipcode or feelings before fetch the data

--- website/app.js
+++ website/app.js
@@ -10,18 +10,12 @@ let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
 let zipCode = '';
 const zipCodeEvent = document.getElementById('zip').addEventListener('input', (e) => {
-    console.log(e.target.value);
     zipCode = e.target.value;
-    console.log('zipCode = ', zipCode);
-
 });
 
 let userFeelings = '';
 const userFeelingInputs = document.getElementById('feelings').addEventListener('input', (e) => {
-    console.log(e.target.value);
     userFeelings = e.target.value;
-    console.log('userFeelings = ', userFeelings);
-
 });
 
 
@@ -29,10 +23,11 @@ const getData = async (url) => {
     await fetch(url + '/getData').then(
         async (res) => {
             const data = await res.json();
-            console.log(data);
             return data
         }
-    )
+    ).catch((error) => {
+        console.log('error in get data function', error);
+    })
 }
 
 const postData = async (url, objectData) => {
@@ -46,10 +41,11 @@ const postData = async (url, objectData) => {
     }).then(
         async (res) => {
             const response = await res.json();
-            console.log(response);
             return response
         }
-    )
+    ).catch((error) => {
+        'error in post Data function', error
+    })
 }
 
 const retrieveData = async () => {
@@ -58,11 +54,11 @@ const retrieveData = async () => {
         // Transform into JSON
         let allData = await request.json();
         allData = JSON.parse(allData);
-        console.log(allData);
+
         // Write updated data to DOM elements
-        document.getElementById('temp').innerHTML = Math.round(allData.temp) + 'degrees';
-        document.getElementById('content').innerHTML = allData.feel;
-        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = 'The temperature: ' + Math.round(allData.temp) + 'degrees';
+        document.getElementById('content').innerHTML = "The user feelings: " + allData.feel;
+        document.getElementById('date').innerHTML = 'The date: ' + allData.date;
     }
     catch (error) {
         console.log('error', error);
@@ -88,15 +84,25 @@ const fetchWeatherData = async (baseUrl, zipCode, apiKey) => {
     }
 }
 
-document.getElementById('generate').addEventListener('click', async() => { 
+document.getElementById('generate').addEventListener('click', async () => {
+    if (!zipCode || !userFeelings) {
+        alert('Enter Zip code and User feelings!');
+        return
+    }
+
     await fetchWeatherData(baseUrl, zipCode, apiKey)
-    .then(
-        (temp)=>{
-            postData(url, {
-                temp: temp,
-                feel: userFeelings,
-                date: newDate
-            })
-        }
-    ).then(()=> retrieveData())
+        .then(
+            (temp) => {
+                postData(url, {
+                    temp: temp,
+                    feel: userFeelings,
+                    date: newDate
+                })
+            }
+        ).then(
+            () => retrieveData()
+        )
+        .catch((err) => {
+            console.log('error in fetching in generate button', error)
+        })
 });
